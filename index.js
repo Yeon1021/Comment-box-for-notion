6a824e9f03203195618dfa653cb410ada484213e
Yeon1021 <64749548+Yeon1021@users.noreply.github.com>
2023-03-18 04:09:12 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+<!-- Vue JS -->
+<script src='https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.17/vue.min.js'>
+ $(document).ready(function(){ 
+ 
+    $(".primaryContained").on('click', function(){
+    $(".comment").addClass("commentClicked");
+  });//end click
+  $("textarea").on('keyup.enter', function(){
+    $(".comment").addClass("commentClicked");
+  });//end keyup
+  });//End Function
+ 
+new Vue({
+    el: "#app",
+    data:{
+       title: 'Add a comment',
+      newItem: '',
+      item: [],
+    },
+    methods:{
+      addItem  (){
+      this.item.push(this.newItem);
+        this.newItem = "";
+      }
+  }
+ 
+  }); 
+</script>
